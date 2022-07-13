fb9665622425f6a46f16ed5b6d2bedcbff7c651c
Carlos Leguizamon <cleguizamon@insiderealestate.com>
2022-07-13 16:14:13 -0300

    Adds login test

--- tests/TestLogin.py
+++ tests/TestLogin.py
@@ -1,8 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support import expected_conditions as ec
 
 
 class TestLogin:
+    TITLE = (By.ID, "welcomeMsg")
 
     def setup_class(self):
         self.driver = webdriver.Chrome(ChromeDriverManager().install())
@@ -10,5 +14,13 @@ class TestLogin:
     def teardown_class(self):
         self.driver.quit()
 
-    def test_abc(self):
-        self.driver.get("https://www.google.com.ar")
+    def test_login(self):
+        self.driver.get("https://practis.co.il/automation/")
+        input_username = self.driver.find_element(By.NAME, "username")
+        input_password = self.driver.find_element(By.NAME, "password")
+        button_login = self.driver.find_element(By.NAME, "login")
+        input_username.send_keys("admin")
+        input_password.send_keys("admin")
+        button_login.click()
+        title = WebDriverWait(self.driver, 5).until(ec.visibility_of_element_located(self.TITLE))
+        assert title.text == "Welcome to the Main page!", "The title is no correct"
